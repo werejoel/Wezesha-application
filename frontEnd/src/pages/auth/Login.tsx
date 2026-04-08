2d62999fe8c02bf61ce7929027b2afd706a13149
werejoel <joelbank481@gmail.com>
2026-04-08 09:56:54 +0300

    j

--- frontEnd/src/pages/auth/Login.tsx
+++ frontEnd/src/pages/auth/Login.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-
+//
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
